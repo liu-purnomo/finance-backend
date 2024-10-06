e9519234b59fbba91fa00c3a2867463b01c14d29
Liu Purnomo <hi@liupurnomo.com>
2024-10-06 08:53:06 +0700

    feat : adding index file

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+require('dotenv').config();
+import cors from 'cors';
+import express from 'express';
+import { errorHandler } from './app/middlewares';
+import router from './app/routes';
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Server is running'
+    });
+});
+
+app.use('/api', router);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
