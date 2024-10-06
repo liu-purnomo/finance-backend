20577fed66cac9ea1e77398714188e846ffde865
Liu Purnomo <hi@liupurnomo.com>
2024-10-06 08:19:46 +0700

    feat : install dev dependecies

--- command-list.sh
+++ command-list.sh
@@ -2,3 +2,4 @@ npm init
 
 npm i express sequelize mysql2 dotenv cors jsonwebtoken nodemailer bcrypt
 
+npm i -D typescript ts-node sequelize-cli @types/node @types/express nodemon @types/jsonwebtoken @types/cors @types/nodemailer
