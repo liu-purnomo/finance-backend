b1b78d380b544ffec128a48a8a9881e24485237a
Liu Purnomo <hi@liupurnomo.com>
2024-10-06 08:36:49 +0700

    feat : change database config"

--- src/db/configs/database.js
+++ src/db/configs/database.js
@@ -1,23 +1,86 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+require('dotenv').config();
+let logNum = 1;
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: Number(process.env.DB_PORT),
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        benchmark: process.env.DB_BENCHMARK ? true : false,
+        logging: (message, benchmark) => {
+            if (process.env.DB_LOGGING) {
+                if (!benchmark) {
+                    return console.info(message);
+                }
+                console.info(`NUMBER:${logNum} ${message} Elapsed time: ${benchmark} ms`);
+                logNum += 1;
+            }
+        },
+        dialectOptions: {
+            charset: 'utf8mb4'
+            // collate: 'utf8mb4_unicode_ci',
+        }
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: (message, benchmark) => {
+            if (process.env.DB_LOGGING) {
+                if (!benchmark) {
+                    return console.info(message);
+                }
+                console.info(`${message} Elapsed time: ${benchmark} ms`);
+            }
+        },
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialectOptions: {
+            charset: 'utf8mb4'
+            // collate: 'utf8mb4_unicode_ci',
+        },
+        benchmark: process.env.DB_BENCHMARK ? true : false
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: (message, benchmark) => {
+            if (process.env.DB_LOGGING) {
+                if (!benchmark) {
+                    return console.info(message);
+                }
+                console.info(`${message} Elapsed time: ${benchmark} ms`);
+            }
+        },
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialectOptions: {
+            charset: 'utf8mb4'
+            // collate: 'utf8mb4_unicode_ci',
+        },
+        benchmark: process.env.DB_BENCHMARK ? true : false
+    }
+};
