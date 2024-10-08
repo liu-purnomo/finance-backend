964c4eb4107d2ee924160de0c92d24273fbfa794
Liu Purnomo <hi@liupurnomo.com>
2024-10-08 20:13:59 +0700

    add route for sub category

--- src/app/routes/v1/finance/index.ts
+++ src/app/routes/v1/finance/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 const router = express.Router();
 
+import category from './category.routes';
+import subCategory from './subCategory.routes';
 import transaction from './transaction.routes';
 import wallet from './wallet.routes';
 
 router.use('/wallet', wallet);
 router.use('/transaction', transaction);
+router.use('/category', category);
+router.use('/sub-category', subCategory);
 
 export default router;
